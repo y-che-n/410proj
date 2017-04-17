f40f6db341d0de51d3277c6274ea756dd9ef6918
Shivali Patel <415035sp@gmail.com>
2017-04-16 19:36:18 -0500

    completeness function

---
+++ backend/src/main/java/com/example/oldchenny/myapplication/backend/Completeness.java
@@ -0,0 +1,9 @@
+package com.example.oldchenny.myapplication.backend;
+
+/**
+ * Created by shivalipatel on 4/16/17.
+ */
+
+public class Completeness {
+    //score completeness and return missing information
+}
